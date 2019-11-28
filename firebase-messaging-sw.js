9eb3b0ee1afda30c80f75267e45a34aab4a7b5a2
Vazer Rahmani <vazer.dpr@gmail.com>
2019-11-28 13:20:05 +0400

    Service Worker
    Added Service Worker

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,31 @@
+// jshint esversion: 6
+
+importScripts('https://www.gstatic.com/firebasejs/7.5.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.5.0/firebase-messaging.js');
+
+firebase.initializeApp({
+  'messagingSenderId': '347541004446'
+});
+
+const messaging = firebase.messaging();
+let notificationAction = "";
+
+// [START background_handler]
+messaging.setBackgroundMessageHandler(function(payload) {
+  const notificationTitle = payload.data.title;
+  notificationAction = payload.data.url;
+
+  const notificationOptions = {
+    body: payload.data.body,
+    icon: payload.data.icon
+  };
+
+  return self.registration.showNotification(notificationTitle, notificationOptions);
+});
+// [END background_handler]
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(self.clients.openWindow(notificationAction));
+});
+
